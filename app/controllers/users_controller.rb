b9035f351e59df2113db6ddcb746c55baf0c714d
Rodrigo Alves <rodrigo.souza@sumup.com>
2019-09-05 00:16:47 -0300

    implements protected URLs on Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, :set_user, only: [:show, :update, :destroy]
+  before_action :authorize_request, except: :create
+  before_action :set_user, except: %i[create index]
 
   # GET /users
   def index
